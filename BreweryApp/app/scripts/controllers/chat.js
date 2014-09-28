a11eaa4ec2c7875a3960d25be45311e5aa437c95
Koen Olijve <k.c.m.olijve@rn.rabobank.nl>
2014-09-28 10:37:06 +0200

    Basis chat applicatie met sockjs

--- BreweryApp/app/scripts/controllers/chat.js
+++ BreweryApp/app/scripts/controllers/chat.js
@@ -8,10 +8,16 @@
  * Controller of the breweryApp
  */
 angular.module('breweryApp')
-  .controller('ChatCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('ChatCtrl', function ($scope) {
+        var sock = new SockJS('http://localhost/chat');
+        $scope.messages = [];
+        $scope.sendMessage = function () {
+            sock.send($scope.messageText);
+            $scope.messageText = '';
+        };
+
+        sock.onmessage = function (e) {
+            $scope.messages.push(e.data);
+            $scope.$apply();
+        };
+    });
