1ae8f4104b38f3275d97da6cd6126f0aa8577ec8
Koen Olijve <k.c.m.olijve@rn.rabobank.nl>
2014-10-10 13:45:08 +0200

    Rest function added to retrieve objects

--- BreweryApp/app/scripts/controllers/rest.js
+++ BreweryApp/app/scripts/controllers/rest.js
@@ -4,4 +4,19 @@ angular.module('breweryApp')
     .controller('RestCtrl', function ($scope, restService) {
         console.log($scope);
         console.log(restService.fullName);
+
+
+        function getUrlAsObject(url) {
+            restService.get(url)
+                .success(function (response) {
+                    if (angular.isObject(response)) {
+                        $scope.test = response;
+                    }
+                });
+        }
+
+        getUrlAsObject('http://localhost:9000');
+        console.log($scope.test);
+
+
     });
