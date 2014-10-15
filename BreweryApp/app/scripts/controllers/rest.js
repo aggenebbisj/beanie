70b078b5636e6981dadbf3cb4880c9c5d39bd1e4
Koen Olijve <k.c.m.olijve@rn.rabobank.nl>
2014-10-15 14:52:49 +0200

    Optimized layout in angular

--- BreweryApp/app/scripts/controllers/rest.js
+++ BreweryApp/app/scripts/controllers/rest.js
@@ -4,19 +4,60 @@ angular.module('breweryApp')
     .controller('RestCtrl', function ($scope, restService) {
         console.log($scope);
         console.log(restService.fullName);
+        var serverUrl = 'http://localhost:9000';
+
+        $scope.operations = {
+            yeast: {
+                'operation': 'add',
+                'name': 'yeast',
+                'unit': 'liter',
+                'value': 0,
+                activate: function (object) {
+                    console.log(object);
+                    sendUrlAsObject(serverUrl + "/" + "ingredient/" + object.name + "/" + object.value + "/" + object.unit);
+                }},
+            temperature: {
+                'operation': 'change',
+                'unit': 'celsius',
+                'name': 'temperature',
+                'value': 0,
+                activate: function (object) {
+                    console.log(object);
+                    sendUrlAsObject(serverUrl + "/" + object.name + "/" + object.value + "/" + object.unit);
+                }},
+            waiting: {
+                'operation': '',
+                'unit': 'minutes',
+                'name': 'wait',
+                'value': 0,
+                activate: function (object) {
+                    console.log(object);
+                    sendUrlAsObject(serverUrl + "/" + object.name + "/" + "PT" + object.value + "M"); //TODO fix to do this automatically
+                }}
+        };
+
 
 
         function getUrlAsObject(url) {
             restService.get(url)
                 .success(function (response) {
                     if (angular.isObject(response)) {
-                        $scope.test = response;
+                        console.log(response);
                     }
                 });
         }
 
-        getUrlAsObject('http://localhost:9000');
-        console.log($scope.test);
+        function sendUrlAsObject(url) {
+            restService.post(url)
+                .success(function (response) {
+                    if (angular.isObject(response)) {
+                        console.log(response);
+                    }
+                });
+        }
+
+//        getUrlAsObject('http://localhost:9000');
+//        console.log($scope.test);
 
 
     });
