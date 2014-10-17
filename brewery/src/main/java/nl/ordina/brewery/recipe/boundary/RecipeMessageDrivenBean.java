62fcbfc5f2b8b3e12e2b04322c25eef70c3033a3
Talip <info@instituutfeelsgood.nl>
2014-10-17 16:16:22 +0200

    replaced script based jms-config by annotations

--- brewery/src/main/java/nl/ordina/brewery/recipe/boundary/RecipeMessageDrivenBean.java
+++ brewery/src/main/java/nl/ordina/brewery/recipe/boundary/RecipeMessageDrivenBean.java
@@ -11,26 +11,35 @@ import javax.jms.TextMessage;
 import javax.json.Json;
 import javax.json.JsonReader;
 import java.io.StringReader;
+import javax.ejb.ActivationConfigProperty;
+import javax.jms.JMSDestinationDefinition;
 
-@MessageDriven(mappedName = "jms/RecipeQueue")
+@JMSDestinationDefinition(name = "java:global/jms/RecipeQueue", interfaceName = "javax.jms.Queue")
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup",
+            propertyValue = "java:global/jms/RecipeQueue"),
+    @ActivationConfigProperty(propertyName = "destinationType",
+            propertyValue = "javax.jms.Queue")
+})
 public class RecipeMessageDrivenBean implements MessageListener {
-  @Inject
-  Brewer brewer;
-  @Inject
-  RecipeParser parser;
 
-  @Override
-  public void onMessage(Message message) {
-    // TODO ugly as hell, fix it
-    if (message instanceof TextMessage) {
-      try {
-        String s = ((TextMessage) message).getText();
-        final JsonReader reader = Json.createReader(new StringReader(s));
-        brewer.brew(parser.parseRecipe(reader.readObject()));
-      } catch (JMSException e) {
-        e.printStackTrace();
-      }
+    @Inject
+    Brewer brewer;
+    @Inject
+    RecipeParser parser;
+
+    @Override
+    public void onMessage(Message message) {
+        // TODO ugly as hell, fix it
+        if (message instanceof TextMessage) {
+            try {
+                String s = ((TextMessage) message).getText();
+                final JsonReader reader = Json.createReader(new StringReader(s));
+                brewer.brew(parser.parseRecipe(reader.readObject()));
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
-
-  }
 }
