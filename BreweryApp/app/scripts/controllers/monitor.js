9eb91393f6c13282eacc3e5940211234933b0772
Remko de Jong <aggenebbisj@gmail.com>
2014-10-19 23:21:01 +0200

    added monitoring for capacity

--- BreweryApp/app/scripts/controllers/monitor.js
+++ BreweryApp/app/scripts/controllers/monitor.js
@@ -2,18 +2,54 @@
 
 angular.module('breweryApp')
     .controller('MonitorCtrl', function ($scope, $rootScope) {
+        $scope.reading = {
+            'capacity': 0,
+            'gauge2': 0,
+            'gauge3': 0,
+            'gauge4': 0
+        }
+
         $scope.error = {
             value: false,
             text: ''
         };
+        
         if (typeof $rootScope.serverUrl === 'undefined') {
             $rootScope.serverUrl = 'http://localhost:8080'
         }
 
-        $scope.readings = {
-            'gauge1': '50',
-            'gauge2': '30',
-            'gauge3': '100',
-            'gauge4': '0'
-        };
+        var contextRoot = window.location.pathname.split('/')[1];
+        var wsUri = "ws://" + document.location.host + "/" + contextRoot + "/brewer";
+        
+        connect();
+
+        function connect() {
+            var websocket = new WebSocket(wsUri);
+            websocket.onopen = function (evt) {
+                $scope.readings = {
+                    'capacity': 0,
+                    'gauge2': 0,
+                    'gauge3': 0,
+                    'gauge4': 0
+                };
+                // writeToScreen("CONNECTED to " + wsUri);
+            };
+
+            websocket.onmessage = function (evt) {
+                // writeToScreen("RECEIVED: " + evt.data);
+                var message = JSON.parse(evt.data);
+                console.log('remko');
+                console.log(message);
+                $scope.readings = {
+                    'capacity': $scope.readings['capacity'] + message.ingredient.volume.value || 0,
+                    'gauge2': '30',
+                    'gauge3': '100',
+                    'gauge4': '0'
+                };
+            };
+
+            websocket.onerror = function (evt) {
+                // writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+            };
+        }
     });
