ffe8265b2a3155f131a63e2b44cb79cbcaa9f808
Remko de Jong <aggenebbisj@gmail.com>
2014-10-20 19:18:03 +0200

    reduced gauges and fixed ingredient monitoring

--- BreweryApp/app/scripts/controllers/monitor.js
+++ BreweryApp/app/scripts/controllers/monitor.js
@@ -4,9 +4,7 @@ angular.module('breweryApp')
     .controller('MonitorCtrl', function ($scope, $rootScope) {
         $scope.reading = {
             'capacity': 0,
-            'gauge2': 0,
-            'gauge3': 0,
-            'gauge4': 0
+            'temperature': 0
         }
 
         $scope.error = {
@@ -28,31 +26,24 @@ angular.module('breweryApp')
             websocket.onopen = function (evt) {
                 $scope.readings = {
                     'capacity': 0,
-                    'gauge2': 0,
-                    'gauge3': 0,
-                    'gauge4': 0
+                    'temperature': 0
                 };
-                // writeToScreen("CONNECTED to " + wsUri);
             };
 
             websocket.onmessage = function (evt) {
-                // writeToScreen("RECEIVED: " + evt.data);
                 var message = JSON.parse(evt.data);
                 console.log('remko');
                 console.log(message);
                 test = message;
-                console.log('pim');
+                console.log('pim2');
                 console.log(message.ingredient.volume);
                 $scope.readings = {
-                    'capacity': $scope.readings['capacity'] + message.ingredient.volume.value || 0,
-                    'gauge2': '30',
-                    'gauge3': '100',
-                    'gauge4': '0'
+                    'capacity': $scope.readings['capacity'] + (message.ingredient.volume.value || 0),
+                    'temperature': (message.kettle && message.kettle.value) || $scope.readings['temperature']
                 };
             };
 
             websocket.onerror = function (evt) {
-                // writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
             };
         }
     });
---
+++ BreweryApp/deploy.sh
@@ -0,0 +1,3 @@
+grunt build
+cp -r dist/ ~/projects/ordina/adambien/brewery/src/main/webapp
+cp -r dist/ ~/projects/ordina/adambien/brewery/target/brewery-1.0-SNAPSHOT/
