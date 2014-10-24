7e95d2e0499387f7c4129db93c1b7d11e2ac73fb
Talip Ozkeles <talip.ozkeles@ordina.nl>
2014-10-24 17:07:57 +0200

    stripped BrewerResource

--- eelco/src/main/java/nl/ordina/beer/brewing/boundary/BrewerResource.java
+++ eelco/src/main/java/nl/ordina/beer/brewing/boundary/BrewerResource.java
@@ -1,81 +1,72 @@
-
 package nl.ordina.beer.brewing.boundary;
 
-import static java.lang.String.format;
 import java.time.Duration;
 import java.util.logging.Logger;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import nl.ordina.beer.brewing.entity.AddIngredient;
 import nl.ordina.beer.entity.Ingredient;
 import nl.ordina.beer.brewing.control.Brewer;
-import nl.ordina.beer.brewing.entity.ChangeTemperature;
-import nl.ordina.beer.brewing.entity.EmptyKettle;
-import nl.ordina.beer.brewing.entity.KeepTemperatureStable;
 import nl.ordina.beer.entity.Kettle;
 import nl.ordina.beer.entity.Temperature;
 
 /**
- * RESTful endpoint for manual brewing.
- * 
+ * RESTful endpoint for manual brewing on the <code>brewer</code> path.
+ *
  * @author Ordina J-Tech
  */
-@Path("brewer")
+// TODO
 public class BrewerResource {
-    
+
     private transient Logger logger = Logger.getLogger(getClass().getName());
-    
-    @Inject 
+
     private Brewer brewer;
 
-    @Inject
     DurationXmlAdapter durationAdapter;
-    
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("ingredients")
+
+    /**
+     * POST method that consumes JSON on the "ingredients" path. Adds a new
+     * AddIngredient BrewerAction to the brewer.
+     *
+     * @param ingredient Marsalled JSON object
+     */
+    // TODO
     public void addIngredient(Ingredient ingredient) {
-        logger.finest(() -> format("Manual: Adding ingredient %s", ingredient));
-        brewer.addAction(new AddIngredient(ingredient));
     }
 
-    @DELETE
-    @Path("ingredients")
+    /**
+     * DELETE method on the "ingredients" path. Adds a new EmptyKettle
+     * BrewerAction to the brewer.
+     */
+    // TODO
     public void emptyKettle() {
-        logger.finest(() -> "Manual: Emptying the kettle...");
-        brewer.addAction(new EmptyKettle());
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("kettle")
+    /**
+     * GET method on the "kettle" path
+     *
+     * @return the Kettle object from the brewer
+     */
+    // TODO
     public Kettle getKettle() {
-        logger.finest(() -> format("Manual: Getting kettle %s", brewer.getKettle()));
-        return brewer.getKettle();
+        return null;
     }
-    
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("kettle/temperature")
+
+    /**
+     * PUT method that consumes JSON on the "kettle/temperature" path. Adds a
+     * new ChangeTemperature BrewerAction to the brewer.
+     *
+     * @param goal new temperature to be changed
+     */
+    // TODO
     public void changeTemperatureTo(Temperature goal) {
-        logger.finest(() -> "Manual: Changing temperature to " + goal);
-        brewer.addAction(new ChangeTemperature(goal));
     }
-    
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("kettle/temperature/stable")
+
+    /**
+     * PUT method that consumes JSON on the "kettle/temperature/stable" path.
+     * Adds a new KeepTemperatureStable BrewerAction to the brewer.
+     *
+     * @param minutes duration to wait
+     */
+    // TODO
     public void keepTemperatureStable(String minutes) {
         Duration duration = durationAdapter.unmarshal(minutes);
-        logger.finest(() -> "Manual: Keeping temperature stable for " + minutes);
-        brewer.addAction(new KeepTemperatureStable(duration));
     }
-    
 }
