926987d531672a41162c63186a4621c807f3d60c
Remko de Jong <aggenebbisj@gmail.com>
2014-10-24 10:45:42 +0200

    refactored action executing

--- eelco/src/main/java/nl/ordina/beer/brewing/control/Brewer.java
+++ eelco/src/main/java/nl/ordina/beer/brewing/control/Brewer.java
@@ -2,15 +2,13 @@ package nl.ordina.beer.brewing.control;
 
 import static java.lang.String.format;
 import java.util.List;
-import nl.ordina.beer.brewing.entity.BrewAction;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import nl.ordina.beer.brewing.entity.BrewActionAddedEvent;
+import nl.ordina.beer.brewing.entity.BrewAction;
 import nl.ordina.beer.brewing.entity.BrewActionCompletedEvent;
 import nl.ordina.beer.entity.Kettle;
 
@@ -19,7 +17,7 @@ public class Brewer {
 
     private transient Logger logger = Logger.getLogger(getClass().getName());
 
-    private final Queue<BrewAction> queue = new ConcurrentLinkedQueue<>();
+    final Queue<BrewAction> queue = new ConcurrentLinkedQueue<>();
 
     @Inject
     private Kettle kettle;
@@ -27,29 +25,17 @@ public class Brewer {
     @Inject
     Event<BrewActionCompletedEvent> actionCompleted;
 
-    @Inject
-    Event<BrewActionAddedEvent> actionAdded;
-    
     public void addActions(List<BrewAction> steps) {
-        steps.stream().forEach((action) -> {
-            queue.add(action);
-        });
+        queue.addAll(steps);
+        executeNextAction();
     }
 
     public void addAction(BrewAction action) {
         logger.info(() -> "Brewer: queue size before adding: " + queue.size());
-        queue.add(action);
-        //TODO: Blocking call, queue is redundant, or make nonblocking (asynchroon) MDB, Queue Observer......
-        if ((action.equals(nextAction()))) executeNextAction();
-    }
-
-    public void onActionCompleted(@Observes BrewActionCompletedEvent actionCompleted) {
-        if (actionCompleted.getAction().isCompleted(kettle)) {
-            queue.remove();
-            logger.info(() -> "Brewer: Action completed. Current queue size: " + queue.size());
+        if (queue.isEmpty()) {
+            queue.add(action);
             executeNextAction();
-        }
-        else executeAction(actionCompleted.getAction());
+        } else queue.add(action); 
     }
 
     public BrewAction nextAction() {
@@ -67,6 +53,8 @@ public class Brewer {
         //TODO: Move into the brew method, then the action raises the action complete event
         logger.info(() -> format("Brewer: action completed. Remaining in queue %s", queue));
         actionCompleted.fire(new BrewActionCompletedEvent(action));
+        queue.remove();
+        executeNextAction();
     }
 
     public Kettle getKettle() {
--- eelco/src/test/java/nl/ordina/beer/brewing/control/BrewerTest.java
+++ eelco/src/test/java/nl/ordina/beer/brewing/control/BrewerTest.java
@@ -5,19 +5,14 @@ import static java.util.Arrays.asList;
 import javax.enterprise.event.Event;
 import static nl.ordina.beer.brewing.control.BrewActionBuilder.defaultAddIngredientAction;
 import static nl.ordina.beer.brewing.control.BrewActionBuilder.defaultChangeTemperatureAction;
-import nl.ordina.beer.brewing.entity.BrewActionAddedEvent;
 import nl.ordina.beer.brewing.entity.BrewActionCompletedEvent;
 import static nl.ordina.beer.entity.EntityBuilder.defaultIngredient;
 import static nl.ordina.beer.entity.EntityBuilder.defaultTemperature;
 import static nl.ordina.beer.entity.EntityBuilder.defaultTemperatureIncrement;
 import nl.ordina.beer.entity.Kettle;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import static org.mockito.Matchers.any;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
@@ -35,65 +30,36 @@ public class BrewerTest {
     @Mock
     private Event<BrewActionCompletedEvent> actionCompleted;
     
-    @Mock
-    private Event<BrewActionAddedEvent> actionAdded;
-    
-    @Test
-    public void should_be_able_to_add_action_to_queue() {
-        sut.addAction(defaultAddIngredientAction());
-        assertThat(sut.nextAction(), is(defaultAddIngredientAction()));
-    }
-    
-    @Ignore
-    @Test
-    public void should_be_able_to_add_multiple_actions_to_queue() {
-        sut.addActions(asList(defaultAddIngredientAction(), defaultChangeTemperatureAction()));
-        assertThat(sut.nextAction(), is(defaultAddIngredientAction()));
-        assertThat(sut.nextAction(), is(defaultChangeTemperatureAction()));
-    }
-    
-    @Ignore
-    @Test
-    public void adding_an_action_should_fire_an_event() {
-        sut.addAction(defaultAddIngredientAction());
-        verify(actionAdded).fire(any(BrewActionAddedEvent.class));
-    }
-    
-    @Ignore
-    @Test
-    public void adding_multiple_actions_should_fire_an_event() {
-        sut.addActions(asList(defaultAddIngredientAction()));
-        verify(actionAdded).fire(any(BrewActionAddedEvent.class));
-    }
-    
     @Test 
     public void should_not_execute_action_when_queue_is_empty() {
         sut.executeNextAction();
         verifyZeroInteractions(kettle);
     }
     
-    @Ignore
     @Test
-    public void should_execute_next_action_when_queue_is_not_empty() {
+    public void should_execute_next_action_immediately_if_queue_is_empty() {
         sut.addAction(defaultAddIngredientAction());
-        sut.addAction(defaultChangeTemperatureAction());
-        sut.executeNextAction();
         verify(kettle).addIngredient(defaultIngredient());
     }
     
     @Test
-    public void should_execute_same_action_if_action_not_fully_completed() {
-        sut.onActionCompleted(new BrewActionCompletedEvent(defaultChangeTemperatureAction()));
-        verify(kettle).changeTemperature(defaultTemperatureIncrement(), defaultTemperature());
+    public void should_not_execute_next_action_immediately_if_queue_is_not_empty() {
+        sut.queue.add(defaultAddIngredientAction());
+        sut.addAction(defaultAddIngredientAction());
+        verifyZeroInteractions(kettle);
     }
     
-    @Ignore
     @Test
-    public void should_fire_event_when_action_is_completed() {
-        sut.actionCompleted = actionCompleted;
+    public void should_fire_event_if_action_completed() {
         sut.addAction(defaultAddIngredientAction());
-        sut.executeNextAction();
         verify(actionCompleted).fire(new BrewActionCompletedEvent(defaultAddIngredientAction()));
+    }
+    
+    @Test
+    public void adding_multiple_action_should_execute_them_all() {
+        sut.addActions(asList(defaultAddIngredientAction(), defaultChangeTemperatureAction()));
+        verify(kettle).addIngredient(defaultIngredient());
+        verify(kettle).changeTemperature(defaultTemperatureIncrement(), defaultTemperature());
     }
     
 }
